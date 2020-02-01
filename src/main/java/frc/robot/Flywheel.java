b4945745c15dfc10a5cc3ec7a72187f1c6f20ff7
mkkaufmann <mkaufmann421@gmail.com>
2020-02-01 11:36:59 -0500

    Add extra shooting methods, including projectile speed option

--- src/main/java/frc/robot/Flywheel.java
+++ src/main/java/frc/robot/Flywheel.java
@@ -12,6 +12,9 @@ class Flywheel {
     static final int ENCODER_TICKS_PER_MOTOR_REVOLUTION = 12;
     static final double MAX_FLYWHEEL_RPM = MAX_MOTOR_RPM/GEARBOX_RATIO;
     static final double ENCODER_TICKS_PER_FLYWHEEL_REVOLUTION = ENCODER_TICKS_PER_MOTOR_REVOLUTION * GEARBOX_RATIO;
+    static final double SPEED_TRANSFER_TO_BALL = 0.4103;
+    static final double WHEEL_DIAMETER = 6.0;
+    static final double WHEEL_CIRCUMFERENCE = Math.PI * WHEEL_DIAMETER;
 
     TalonSRX flywheelMaster = new TalonSRX(0);
     VictorSPX flywheelFollower = new VictorSPX(1);
@@ -30,14 +33,37 @@ class Flywheel {
         flywheelMaster.set(ControlMode.PercentOutput, output);
     }
 
-    public void runVelocity(double velocity){
+    private void runVelocity(double velocity){
         System.out.println("Encoder velocity" + (flywheelMaster.getSelectedSensorVelocity()));
         System.out.println("Encoder difference from velocity " + (Math.abs(velocity) - (Math.abs(flywheelMaster.getSelectedSensorVelocity()))));
         flywheelMaster.set(ControlMode.Velocity, velocity);
     }
 
-    public void runRPM(double rpm){
-        runVelocity(rpm/600*ENCODER_TICKS_PER_FLYWHEEL_REVOLUTION);
+    private void runRotationsPer100ms(double rotationsPer100ms){
+        runVelocity(rotationsPer100ms * ENCODER_TICKS_PER_FLYWHEEL_REVOLUTION);
     }
 
+    public void runRPM(double rpm){
+        runRotationsPer100ms(rpmToRotationsPer100ms(rpm));
+    }
+
+    public void shootProjectileAtSpeed(double inchesPerSecond){
+        runRotationsPer100ms(getDesiredSurfaceSpeed(inchesPerSecondToRotationsPer100ms(inchesPerSecond)));
+    }
+
+    private double rpmToRotationsPer100ms(double rpm){
+        return rpm/600;
+    }
+
+    private double inchesToRotations(double inches){
+        return inches/WHEEL_CIRCUMFERENCE;
+    }
+
+    private double inchesPerSecondToRotationsPer100ms(double inchesPerSecond){
+        return inchesToRotations(inchesPerSecond)/10;
+    }
+
+    private double getDesiredSurfaceSpeed(double projectileSpeed){
+        return projectileSpeed/SPEED_TRANSFER_TO_BALL; 
+    }
 }
