378e6f4eca7e33778fc72cad391d497965bc8470
mkkaufmann <mkaufmann421@gmail.com>
2020-02-02 19:55:23 -0500

    Added shoot from distance using physics method

--- src/main/java/frc/robot/Flywheel.java
+++ src/main/java/frc/robot/Flywheel.java
@@ -15,6 +15,12 @@ class Flywheel {
     static final double SPEED_TRANSFER_TO_BALL = 0.4103;
     static final double WHEEL_DIAMETER = 6.0;
     static final double WHEEL_CIRCUMFERENCE = Math.PI * WHEEL_DIAMETER;
+    static final double FLYWHEEL_ANGLE_RADIANS = Math.toRadians(18);
+    static final double FIRING_HEIGHT_INCHES = 23;
+    static final double MAX_FLYWEEL_VELOCITY_INCHES_PER_SECOND = WHEEL_CIRCUMFERENCE * (MAX_FLYWHEEL_RPM / 60);
+    static final double MAX_PROJECTILE_VELOCITY_INCHES_PER_SECOND = MAX_FLYWEEL_VELOCITY_INCHES_PER_SECOND * SPEED_TRANSFER_TO_BALL;
+    static final double VELOCITY_ERROR = 0.02;
+    static final double HEIGHT_OF_TARGET_INCHES = 98.25;
 
     TalonSRX flywheelMaster = new TalonSRX(0);
     VictorSPX flywheelFollower = new VictorSPX(1);
@@ -49,6 +55,24 @@ class Flywheel {
 
     public void shootProjectileAtSpeed(double inchesPerSecond){
         runRotationsPer100ms(getDesiredSurfaceSpeed(inchesPerSecondToRotationsPer100ms(inchesPerSecond)));
+    }
+
+    public void shootUsingPhysicsFromDistance(double inches){
+        double targetSpeedInchesPerSecond;
+        //trajectory math for finding an optimal speed, pulled from a trajectory spreadsheet
+        //variables just break the problem into chunks
+        double D = MAX_PROJECTILE_VELOCITY_INCHES_PER_SECOND/(1 + VELOCITY_ERROR);
+        try{
+            double A = inches * inches * 2 * 386.088583;
+            double B = Math.tan(FLYWHEEL_ANGLE_RADIANS) * Math.tan(FLYWHEEL_ANGLE_RADIANS);
+            double E = (inches * Math.tan(FLYWHEEL_ANGLE_RADIANS)) - (HEIGHT_OF_TARGET_INCHES - FIRING_HEIGHT_INCHES);
+            double F = (A * B) / E;
+            double G = 0.5 * 1.0/Math.sin(FLYWHEEL_ANGLE_RADIANS) * Math.sqrt(F);
+            targetSpeedInchesPerSecond = Math.min(G, D);
+        }catch(Exception e){ 
+            targetSpeedInchesPerSecond = D;
+        };
+        shootProjectileAtSpeed(targetSpeedInchesPerSecond);
     }
 
     private double rpmToRotationsPer100ms(double rpm){
